213c5a6506218614f3ca4acd5e2bf1e49305c443
xrjam <xrjam@Homepc>
2025-06-27 18:25:10 +0200

    Se borra para reinicio como proyecto en Amazon Amplify

--- ordenes.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Cambios realizados a través de una funcion javascript en archivo externo.";
-  document.getElementById("demo1").innerHTML = "Se me olvidaba, los colores y el formato demuestran la vinculacion del archivo css correctamente.";
-  
-  
-}
